66f8db6c2f5e4186e34c6eab78e663af8339a585
IevgenBabak <111040681+IevgenBabak@users.noreply.github.com>
2023-05-07 21:52:39 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import ContactForm from './ContactForm';
+import Filter from './Filter';
+import ContactList from './ContactList';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="Phonebook_box">
+      <h1 className="Phonebook_title">Phonebook</h1>
+      <ContactForm />
+      <h2 className="Phonebook_second-title">Contacts</h2>
+      <Filter />
+
+      <ContactList />
+    </div>
+  );
+};
+export default App;
